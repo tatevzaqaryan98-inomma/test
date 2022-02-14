b2fc81e9c9fe1f184db9b509611b475b8dbf785f
tatevzaqaryan98-inomma <tatev.zaqaryan@inomma.com>
2022-02-14 19:35:57 +0400

    add won func

--- ticTacToe/Game.js
+++ ticTacToe/Game.js
@@ -19,68 +19,56 @@ class Game {
   playerQueue() {
     window.play = (row, column) => {
       this.board[row][column] = this.player;
+      this.checkWinnerDecide();
+      console.table(this.board);
       if (this.player === "x") {
         this.player = "0";
       } else {
         this.player = "x";
       }
-      console.table(this.board);
     };
   }
 
   checkWinnerDecide() {
-    for (let i = 0; i < this.column - 2; i++) {
+    for (let i = 0; i < this.colum - 2; i++) {
       for (let j = 0; j < this.rows; j++) {
         if (
-          this.board[i][j] === this.player &&
-          this.board[i + 1][j] === this.player &&
-          this.board[i + 2][j] === this.player
+          this.board[i][j] === this.board[i + 1][j] &&
+          this.board[i + 1][j] === this.board[i + 2][j]
         ) {
-          return true;
+          return console.log(`${this.player} winner game`);
         }
       }
     }
 
     //  test
-
-    for (let i = 0; i < this.column; i++) {
+    console.log(this.board);
+    for (let i = 0; i < this.colum; i++) {
       for (let j = 0; j < this.rows - 2; j++) {
         if (
-          this.board[i][j] === this.player &&
-          this.board[i][j + 1] === this.player &&
-          this.board[i][j + 2] === this.player
+          this.board[i][j] === this.board[i][j + 1] &&
+          this.board[i][j + 1] === this.board[i][j + 2]
         ) {
-          return true;
+          return console.log(`${this.player} winner game`);
         }
       }
     }
 
     if (
-      this.board[0][0] === this.player &&
-      this.board[1][1] === this.player &&
-      this.board[2][2] === this.player
+      this.board[0][0] === this.board[1][1] &&
+      this.board[1][1] === this.board[2][2]
     ) {
-      return true;
+      return console.log(`${this.player} winner game`);
     }
     if (
-      this.board[0][2] === this.player &&
-      this.board[1][1] === this.player &&
-      this.board[2][0] === this.player
+      this.board[0][2] === this.board[1][1] &&
+      this.board[1][1] === this.board[2][0]
     ) {
-      return true;
+      return console.log(`${this.player} winner game`);
     }
-  }
-
-  winnerDecide() {
-    const result = this.checkWinnerDecide();
-    if (result) {
-      console.log(`${this.player} winner game`);
-    }
-    console.log(this.board[0][0] === this.player);
   }
 }
 
 let game = new Game();
 game.createBoard();
 game.playerQueue();
-game.winnerDecide();
