bcc1b399229536203f7d6b6f67a786268cd7a91a
tatevzaqaryan98-inomma <tatev.zaqaryan@inomma.com>
2022-02-15 01:23:50 +0400

    is full

--- ticTacToe/Game.js
+++ ticTacToe/Game.js
@@ -10,17 +10,33 @@ class Game {
     this.boardStr;
     this.player = "x";
   }
-  createBoard() {
-    for (let i = 0; i < this.colum; i++) {
-      this.boardStr = this.board[i].join(" ");
-      console.log(`${i} ${this.boardStr}`);
-    }
+  printBoard() {
+    console.table(this.board);
   }
   playerQueue() {
     window.play = (row, column) => {
-      this.board[row][column] = this.player;
-      this.checkWinnerDecide();
+      if (this.board[row][column] != "-") {
+        return console.log("this cell is not empty");
+      } else {
+        this.board[row][column] = this.player;
+      }
+
       console.table(this.board);
+      this.checkWinnerDecide();
+
+      if (this.isNotFool()) {
+        console.log("board is not full");
+      } else {
+        console.log("board is full");
+
+        this.board = [
+          ["-", "-", "-"],
+          ["-", "-", "-"],
+          ["-", "-", "-"],
+        ];
+        this.printBoard();
+      }
+
       if (this.player === "x") {
         this.player = "0";
       } else {
@@ -34,20 +50,20 @@ class Game {
       for (let j = 0; j < this.rows; j++) {
         if (
           this.board[i][j] === this.board[i + 1][j] &&
-          this.board[i + 1][j] === this.board[i + 2][j]
+          this.board[i + 1][j] === this.board[i + 2][j] &&
+          this.board[i][j] != "-"
         ) {
           return console.log(`${this.player} winner game`);
         }
       }
     }
 
-    //  test
-    console.log(this.board);
     for (let i = 0; i < this.colum; i++) {
       for (let j = 0; j < this.rows - 2; j++) {
         if (
           this.board[i][j] === this.board[i][j + 1] &&
-          this.board[i][j + 1] === this.board[i][j + 2]
+          this.board[i][j + 1] === this.board[i][j + 2] &&
+          this.board[i][j] != "-"
         ) {
           return console.log(`${this.player} winner game`);
         }
@@ -56,19 +72,32 @@ class Game {
 
     if (
       this.board[0][0] === this.board[1][1] &&
-      this.board[1][1] === this.board[2][2]
+      this.board[1][1] === this.board[2][2] &&
+      this.board[0][0] != "-"
     ) {
       return console.log(`${this.player} winner game`);
     }
     if (
       this.board[0][2] === this.board[1][1] &&
-      this.board[1][1] === this.board[2][0]
+      this.board[1][1] === this.board[2][0] &&
+      this.board[0][2] != "-"
     ) {
       return console.log(`${this.player} winner game`);
     }
+    return false;
+  }
+  isNotFool() {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board.length; j++) {
+        if (this.board[i][j] === "-") {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
 
 let game = new Game();
-game.createBoard();
+game.printBoard();
 game.playerQueue();
